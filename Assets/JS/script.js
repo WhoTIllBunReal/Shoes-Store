4cbc6158637dcdd9ef3c641fc3ae50476bbf06b4
Phạm Quốc Tuấn <Kuuhaku986@gmail.com>
2023-12-22 14:27:22 +0700

    .

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -11,3 +11,29 @@ $(document).ready(function(){
     $('.subimg').height(subimg);
 })
 
+$('#blog-slider-slick').slick({
+    slidesToShow: 3,        // Number of slides to show at a time
+    slidesToScroll: 2,      // Number of slides to scroll
+    autoplay: false,         // Enable autoplay
+    autoplaySpeed: 1000,    // Autoplay interval in milliseconds
+    arrows: true,           // Display navigation arrows
+    dots: true,
+  
+  
+    responsive: [
+      {
+        breakpoint: 800,
+        settings: { 
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,      // Number of slides to scroll
+  
+        }
+      }
+    ]
+  });
