6afa8c3461b16d957c2a7fc71ae9505afda205bf
Phạm Quốc Tuấn <Kuuhaku986@gmail.com>
2023-12-22 09:26:49 +0700

    .

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,10 +1,10 @@
-$(document).ready(function () {
-    // When the menu button is clicked
-    $("#btn-menu").click(function () {
-        // Toggle the visibility of the navigation
-        $(".hd-nav").slideToggle();
-    });
-});
+// $(document).ready(function () {
+//     // When the menu button is clicked
+//     $("#btn-menu").click(function () {
+//         // Toggle the visibility of the navigation
+//         $(".hd-nav").slideToggle();
+//     });
+// });
 
 $(document).ready(function(){
     var subimg=$('.subimg').height();
